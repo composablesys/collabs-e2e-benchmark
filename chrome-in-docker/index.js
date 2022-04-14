935894071794964cf19b27d516b8c64ffee4c60e
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2022-04-14 18:05:40 -0400

    add stats logs

--- chrome-in-docker/index.js
+++ chrome-in-docker/index.js
@@ -1,31 +1,54 @@
-const puppeteer = require('puppeteer-core')
+const puppeteer = require("puppeteer");
+const osu = require("node-os-utils");
 
-;(async () => {
+(async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: 'chromium',
-    userDataDir: '/home/root',
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--ignore-certificate-errors',
-    ],
-  })
+    // executablePath: "chromium",
+    // userDataDir: "/home/root",
+    // args: [
+    //   "--no-sandbox",
+    //   "--disable-setuid-sandbox",
+    //   "--disable-dev-shm-usage",
+    //   "--ignore-certificate-errors",
+    // ],
+  });
 
-  const page = await browser.newPage()
+  const page = await browser.newPage();
 
-  page.on('console', msg => console.log(new Date().toISOString(), msg.text()))
+  page.on("console", (msg) =>
+    console.log("APP", new Date().toISOString(), msg.text())
+  );
 
-  page.on('pageerror', err => {
-    console.log('[ERROR] ', err.name, err.message, err.stack)
-  })
+  page.on("pageerror", (err) => {
+    console.log("[ERROR] ", err.name, err.message, err.stack);
+  });
 
-  console.log(`Loading page: ${process.env.URL}`)
-  await page.goto(process.env.URL)
+  console.log(`Loading page: ${process.env.URL}`);
+  await page.goto(process.env.URL);
+  console.log("Loaded", new Date().toISOString());
 
-  process.on('SIGTERM', () => {
-    browser.close()
-    process.exit(0)
-  })
-})()
+  // Stats
+  setInterval(() => {
+    console.log(
+      "CPU",
+      new Date().toISOString(),
+      JSON.stringify(osu.cpu.average())
+    );
+    osu.mem
+      .info()
+      .then((info) =>
+        console.log("MEMORY", new Date().toISOString(), JSON.stringify(info))
+      );
+    osu.netstat
+      .stats()
+      .then((info) =>
+        console.log("NETWORK", new Date().toISOString(), JSON.stringify(info))
+      );
+  }, 1000);
+
+  process.on("SIGTERM", () => {
+    browser.close();
+    process.exit(0);
+  });
+})();
--- site/src/server/static.ts
+++ site/src/server/static.ts
@@ -1,5 +1,6 @@
 import express = require("express");
 import path = require("path");
+import osu = require("node-os-utils");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -7,3 +8,30 @@ const port = process.env.PORT || 8080;
 // Serve build/site under /.
 app.use("/", express.static(path.join(__dirname, "../site")));
 app.listen(port, () => console.log(`Listening at http://localhost:${port}/`));
+
+// Stats
+setInterval(() => {
+  console.log(
+    "SERVER_CPU",
+    new Date().toISOString(),
+    JSON.stringify(osu.cpu.average())
+  );
+  osu.mem
+    .info()
+    .then((info) =>
+      console.log(
+        "SERVER_MEMORY",
+        new Date().toISOString(),
+        JSON.stringify(info)
+      )
+    );
+  osu.netstat
+    .stats()
+    .then((info) =>
+      console.log(
+        "SERVER_NETWORK",
+        new Date().toISOString(),
+        JSON.stringify(info)
+      )
+    );
+}, 1000);
